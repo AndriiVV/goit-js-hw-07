3fcd0ca9e672a9d685f7871e06cc8c6f19da5ec6
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2021-12-22 20:30:29 +0200

    Task 01-02
    Task 01-02 were completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+	gallery: document.querySelector(".gallery"),
+};
+
+refs.gallery.addEventListener("click", handleClick);
+
+function handleClick(event) {
+	event.preventDefault();
+	if (event.target.nodeName !== "IMG") return;
+
+	const instance = basicLightbox.create(
+		`<img src="${event.target.dataset.source}">`
+	);
+	instance.show();
+}
+
+createImageGallery();
+
+function createImageGallery() {
+	const newGalleryItems = galleryItems
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+      </a>
+      </div>`;
+		})
+		.join("");
+
+	refs.gallery.insertAdjacentHTML("afterbegin", newGalleryItems);
+}
